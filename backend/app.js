a484490aa2fe14472bfa251c4d659a648318a021
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-06-24 18:16:59 -0300

    Post participants implemented

---
+++ backend/app.js
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+import dayjs from "dayjs";
+import { MongoClient, ObjectId } from 'mongodb';
+import joi from "joi";
+
+
+
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+    db = mongoClient.db("bate-papo-uol");
+});
+
+const participantSchema = joi.object({
+    name: joi.string().required(),
+  });
+
+server.post("/participants", async (req, res) =>{
+    const participant = req.body;
+    const validation = participantSchema.validate(participant,{abortEarly: false});
+    participant.lastStatus = Date.now()
+    const entry = {from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(participant.lastStatus).format("HH:mm:ss")}
+    if(validation.error){
+        res.status(422).send(validation.error.details.map(item => item.message))
+        return
+    }
+
+    
+    
+
+    try {
+        const existingName = await db.collection("participants").findOne({name: participant.name})
+        if (existingName) {
+            return res.sendStatus(409);
+          }
+      
+        await db.collection("participants").insertOne(participant)
+        await db.collection("messages").insertOne(entry)
+        res.sendStatus(201)
+    } catch(error){
+        console.error(error);
+        res.sendStatus(500)
+    }
+    
+  } )
+
+
+  server.listen(5000, () => {
+    console.log('Server is litening on port 5000.');
+  });
+  
